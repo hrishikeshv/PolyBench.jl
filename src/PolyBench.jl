e0ab206fc2b568c50a1729eb81cbcd7579bcb326
Hrishikesh Vaidya <hrishikeshv95@gmail.com>
2016-10-24 13:25:59 +0530

    PolyBench.jl modified

--- src/PolyBench.jl
+++ src/PolyBench.jl
@@ -4,16 +4,32 @@ using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
 const BENCH_DIRS = ["datamining",
-					"polyNN/CNN",
+                    "polyNN/CNN",
+                    "polyNN/RNN",
                     "linear-algebra/blas",
                     "linear-algebra/kernels",
                     "linear-algebra/solvers",
                     "medley",
                     "stencils"]
 
-function load!()
+function load(bench_dir::AbstractString)
+	print("loading $bench_dir...")
+	bench_dir_path = joinpath(dirname(@__FILE__), bench_dir)
+	for bench_file in filter(x -> endswith(x, "jl"), readdir(bench_dir_path))
+		bench_file_path = joinpath(bench_dir_path, bench_file)
+		bench_name = bench_file[1:end-3]
+		print("loading $bench_name...")
+		tic()
+		eval(PolyBench, :(include($bench_file_path)))
+		load_time = toq()
+		println("done (took $load_time) seconds)")
+	end
+    return SUITE
+end
+
+function loadall!()
     for bench_dir in BENCH_DIRS
-		print("loading $bench_dir...")
+        print("loading $bench_dir...")
         bench_dir_path = joinpath(dirname(@__FILE__), bench_dir)
         for bench_file in filter(x -> endswith(x, "jl"), readdir(bench_dir_path))
             bench_file_path = joinpath(bench_dir_path, bench_file)
