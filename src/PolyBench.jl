b421443e396078d215d3c124b3abe717e98b234e
Hrishikesh Vaidya <hrishikeshv95@gmail.com>
2016-10-24 19:03:07 +0530

    PolyBench.jl modified

--- src/PolyBench.jl
+++ src/PolyBench.jl
@@ -6,7 +6,7 @@ const SUITE = BenchmarkGroup()
 const BENCH_DIRS = ["datamining",
                     "polyNN/CNN",
                     "polyNN/RNN",
-					"polyNN/LSTM",
+                    "polyNN/LSTM",
                     "linear-algebra/blas",
                     "linear-algebra/kernels",
                     "linear-algebra/solvers",
@@ -14,17 +14,17 @@ const BENCH_DIRS = ["datamining",
                     "stencils"]
 
 function load!(bench_dir::AbstractString)
-	print("loading $bench_dir...")
-	bench_dir_path = joinpath(dirname(@__FILE__), bench_dir)
-	for bench_file in filter(x -> endswith(x, "jl"), readdir(bench_dir_path))
-		bench_file_path = joinpath(bench_dir_path, bench_file)
-		bench_name = bench_file[1:end-3]
-		print("loading $bench_name...")
-		tic()
-		eval(PolyBench, :(include($bench_file_path)))
-		load_time = toq()
-		println("done (took $load_time) seconds)")
-	end
+    print("loading $bench_dir...")
+    bench_dir_path = joinpath(dirname(@__FILE__), bench_dir)
+    for bench_file in filter(x -> endswith(x, "jl"), readdir(bench_dir_path))
+        bench_file_path = joinpath(bench_dir_path, bench_file)
+        bench_name = bench_file[1:end-3]
+        print("loading $bench_name...")
+        tic()
+        eval(PolyBench, :(include($bench_file_path)))
+        load_time = toq()
+        println("done (took $load_time) seconds)")
+    end
     return SUITE
 end
 
